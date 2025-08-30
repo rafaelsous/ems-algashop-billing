ba77761f812c5c305b9c7196f94755729c3bf28e
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-08-30 00:13:00 -0300

    EMS-N-02-16.02 - Building billing project.

---
+++ src/test/java/com/rafaelsousa/algashop/billing/BillingApplicationIT.java
@@ -0,0 +1,13 @@
+package com.rafaelsousa.algashop.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
