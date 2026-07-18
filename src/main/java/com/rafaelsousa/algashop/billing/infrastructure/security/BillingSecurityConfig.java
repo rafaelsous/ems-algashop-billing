f71bcc7ae700873303b404e613dcb6aa1816ee5a
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-07-18 17:42:01 -0300

    EMS-N-05-15.13 - Setting up billing microservice to use AWS Secrets Manager and Parameter Store.

--- src/main/java/com/rafaelsousa/algashop/billing/infrastructure/security/BillingSecurityConfig.java
+++ src/main/java/com/rafaelsousa/algashop/billing/infrastructure/security/BillingSecurityConfig.java
@@ -15,19 +15,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class BillingSecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf(AbstractHttpConfigurer::disable)
-			.cors(AbstractHttpConfigurer::disable)
-			.sessionManagement(sessionManagement ->
-				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/actuator/health/**", "/api/v1/webhooks/**").permitAll()
-				.anyRequest().authenticated())
-			.oauth2ResourceServer(oauth2 ->
-				oauth2.jwt(Customizer.withDefaults()));
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(
+                                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers("/actuator/health/**", "/api/v1/webhooks/**")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
-		return http.build();
-	}
+        return http.build();
+    }
 }
